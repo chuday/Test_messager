aca534a029587e3838a3d06597f68587a48400ee
chuday <mikhail_chudaew505@mail.ru>
2023-05-16 09:47:21 +0500

    add proj

---
+++ testMessager/testMessager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testMessager
+//
+//  Created by Mikhail Chudaev on 16.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
